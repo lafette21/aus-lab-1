5d532f1c2a3d9967f3511e888484c8f24f0efb26
lafette21 <viragh.akos21@gmail.com>
2024-05-02 19:33:56 +0200

    feat: Point registration

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,3 +1,4 @@
 find_package(fmt REQUIRED)
 find_package(PCL REQUIRED COMPONENTS common filters io segmentation)
+find_package(range-v3 REQUIRED)
 find_package(spdlog REQUIRED)
--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,10 @@
 #include "cylinder.hh"
 #include "utils.hh"
 
+#include <Eigen/Core>
+#include <Eigen/Dense>
 #include <nova/vec.h>
+#include <nova/utils.h>
 #include <fmt/format.h>
 #include <pcl/common/common.h>
 #include <pcl/common/transforms.h>
@@ -9,11 +12,14 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
+#include <range/v3/range/conversion.hpp>
 #include <spdlog/spdlog.h>
 
 #include <array>
+#include <concepts>
 #include <cstdlib>
 #include <future>
+#include <numbers>
 #include <ranges>
 #include <span>
 
@@ -27,14 +33,39 @@ inline auto& init(const std::string& name) {
     return logger;
 }
 
+template <std::floating_point T = float>
+[[nodiscard]] auto gen_circle(T x, T y, T z, T r, std::size_t num_points = 100)
+        -> std::vector<nova::vec3<T>>
+{
+    std::vector<nova::vec3<T>> ret;
+    ret.reserve(num_points);
+
+    const auto angles = nova::linspace<T>(nova::range<T>{ 0.f, 2 * std::numbers::pi_v<T> }, num_points);
+
+    for (const auto& angle : angles) {
+        const auto x_ = x + r * std::cos(angle);
+        const auto y_ = y + r * std::sin(angle);
+
+        ret.emplace_back(x_, y_, z);
+    }
+
+    return ret;
+}
+
+template <std::floating_point T = float>
+[[nodiscard]] auto gen_circle(nova::vec4<T> params, std::size_t num_points = 100)
+        -> std::vector<nova::vec3<T>>
+{
+    return gen_circle(params.x(), params.y(), params.z(), params.w(), num_points);
+}
+
 [[nodiscard]] auto extract_cylinder(const pcl::PointCloud<pcl::PointXYZRGB>& cloud)
         -> std::tuple<nova::Vec4f, pcl::PointCloud<pcl::PointXYZRGB>, std::vector<nova::Vec3f>>
 {
-    auto tmp = cloud
-             | std::views::transform([](const auto& elem) { return nova::Vec3f { elem.x, elem.y, elem.z }; })
-             | std::views::filter([](const auto& elem) { return elem != nova::Vec3f { 0, 0, 0 }; });
-
-    std::vector<nova::Vec3f> points(std::begin(tmp), std::end(tmp));
+    const auto points = cloud
+                      | std::views::transform([](const auto& elem) { return nova::Vec3f { elem.x, elem.y, elem.z }; })
+                      | std::views::filter([](const auto& elem) { return elem != nova::Vec3f { 0, 0, 0 }; })
+                      | ranges::to<std::vector>();
 
     const auto cylinder_params = estimate_cylinder_RANSAC(points, 0.07f, 5'000);
     const auto differences = calculate_RANSAC_diffs(points, cylinder_params, 0.07f);
@@ -56,23 +87,6 @@ inline auto& init(const std::string& name) {
         rest
     };
 }
-
-// [[nodiscard]] auto calc_curr_pose(
-    // float prev_velocity,
-    // float prev_angular_velocity,
-    // float prev_x,
-    // float prev_y,
-    // float prev_theta,
-    // float prev_side_slipping = 0
-// )
-        // -> std::tuple<float, float, float>
-// {
-    // return {
-        // prev_x + prev_velocity * std::cos(prev_theta + prev_angular_velocity / 2 + prev_side_slipping),
-        // prev_y + prev_velocity * std::sin(prev_theta + prev_angular_velocity / 2 + prev_side_slipping),
-        // prev_theta + prev_angular_velocity
-    // };
-// }
 
 [[nodiscard]] auto quarter_cloud(const pcl::PointCloud<pcl::PointXYZRGB>& cloud)
         -> std::array<pcl::PointCloud<pcl::PointXYZRGB>, 4>
@@ -111,7 +125,7 @@ inline auto& init(const std::string& name) {
 }
 
 struct trafo {
-    // R;
+    Eigen::Matrix3f R;
     nova::Vec3f offset1;
     nova::Vec3f offset2;
     float scale;
@@ -137,133 +151,60 @@ auto register_(const auto& pts1, const auto& pts2)
         offset2 += p2;
     }
 
-    offset1 /= num_pts;
-    offset2 /= num_pts;
+    offset1 /= static_cast<float>(num_pts);
+    offset2 /= static_cast<float>(num_pts);
 
     ret.offset1 = offset1;
     ret.offset2 = offset2;
 
+    Eigen::Matrix3f H = Eigen::Matrix3f::Zero();
 
+    for (std::size_t i = 0; i < num_pts; ++i) {
+        const auto p1 = pts1[i] - offset1;
+        const auto p2 = pts2[i] - offset2;
+
+        H(0, 0) += p2.x() * p1.x();
+        H(0, 1) += p2.x() * p1.y();
+        H(0, 2) += p2.x() * p1.z();
+
+        H(1, 0) += p2.y() * p1.x();
+        H(1, 1) += p2.y() * p1.y();
+        H(1, 2) += p2.y() * p1.z();
+
+        H(2, 0) += p2.z() * p1.x();
+        H(2, 1) += p2.z() * p1.y();
+        H(2, 2) += p2.z() * p1.z();
+    }
+
+    Eigen::JacobiSVD<Eigen::Matrix3f> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+    Eigen::Matrix3f u = svd.matrixU();
+    Eigen::Matrix3f vt = svd.matrixV().transpose();
+
+    ret.R = vt.transpose() * u.transpose();
+
+    float num = 0;
+    float denom = 0;
+
+    for (std::size_t i = 0; i < num_pts; ++i) {
+        const auto p1 = pts1[i] - offset1;
+        const auto p2 = pts2[i] - offset2;
+
+        Eigen::Vector3f p2_;
+        p2_(0) = p2.x();
+        p2_(1) = p2.y();
+        p2_(2) = p2.z();
+
+        const auto p2_rot = ret.R * p2_;
+
+        num += p1.x() * p2_rot(0) + p1.y() * p2_rot(1) + p1.z() * p2_rot(2);
+        denom += p2_rot(0) * p2_rot(0) + p2_rot(1) * p2_rot(1) + p2_rot(2) * p2_rot(2);
+    }
+
+    ret.scale = num / denom;
+
+    return ret;
 }
-
-// typedef struct Trafo{
-// Mat rot;
-// float scale;
-// Point3f offset1,offset2;
-// } Trafo;
-
-// Trafo registration(vector<Point3f>& pts1, vector<Point3f>& pts2){
-
-// Trafo ret;
-
-	// int NUM=pts1.size();
-
-	// //Subtract offsets
-
-	// Point3d offset1(0.0,0.0,0.0);
-	// Point3d offset2(0.0,0.0,0.0);
-
-	// for (int i=0;i<NUM;i++){
-		// Point3f v1=pts1[i];
-		// Point3f v2=pts2[i];
-
-		// offset1.x+=v1.x;
-		// offset1.y+=v1.y;
-		// offset1.z+=v1.z;
-
-		// offset2.x+=v2.x;
-		// offset2.y+=v2.y;
-		// offset2.z+=v2.z;
-	// }
-
-	// offset1.x/=NUM;
-	// offset1.y/=NUM;
-	// offset1.z/=NUM;
-
-	// offset2.x/=NUM;
-	// offset2.y/=NUM;
-	// offset2.z/=NUM;
-
-	// ret.offset1.x=offset1.x;
-	// ret.offset1.y=offset1.y;
-	// ret.offset1.z=offset1.z;
-
-	// ret.offset2.x=offset2.x;
-	// ret.offset2.y=offset2.y;
-	// ret.offset2.z=offset2.z;
-
-	// Mat H=Mat::zeros(3,3,CV_32F);
-	// for (int i=0;i<NUM;i++){
-		// Point3f v1=pts1[i];
-		// Point3f v2=pts2[i];
-
-		// float x1=v1.x-offset1.x;
-		// float y1=v1.y-offset1.y;
-		// float z1=v1.z-offset1.z;
-
-		// float x2=v2.x-offset2.x;
-		// float y2=v2.y-offset2.y;
-		// float z2=v2.z-offset2.z;
-
-		// H.at<float>(0,0)+=x2*x1;
-		// H.at<float>(0,1)+=x2*y1;
-		// H.at<float>(0,2)+=x2*z1;
-
-		// H.at<float>(1,0)+=y2*x1;
-		// H.at<float>(1,1)+=y2*y1;
-		// H.at<float>(1,2)+=y2*z1;
-
-		// H.at<float>(2,0)+=z2*x1;
-		// H.at<float>(2,1)+=z2*y1;
-		// H.at<float>(2,2)+=z2*z1;
-	// }
-
-
-	// Mat w(3,3,CV_32F);
-	// Mat u(3,3,CV_32F);
-	// Mat vt(3,3,CV_32F);
-
-	// SVD::compute(H,w,u, vt);
-
-	// Mat rot=vt.t()*u.t();
-	// ret.rot=rot;
-
-	// float numerator=0.0;
-	// float denominator=0.0;
-
-	// for (int i=0;i<NUM;i++){
-		// Point3f v1=pts1[i];
-		// Point3f v2=pts2[i];
-
-		// float x1=v1.x-offset1.x;
-		// float y1=v1.y-offset1.y;
-		// float z1=v1.z-offset1.z;
-
-
-		// Mat p2(3,1,CV_32F);
-
-		// p2.at<float>(0,0)=v2.x-offset2.x;
-		// p2.at<float>(1,0)=v2.y-offset2.y;
-		// p2.at<float>(2,0)=v2.z-offset2.z;
-
-		// p2=rot*p2;
-
-		// float x2=p2.at<float>(0,0);
-		// float y2=p2.at<float>(1,0);
-		// float z2=p2.at<float>(2,0);
-
-
-		// numerator+=x1*x2+y1*y2+z1*z2;
-		// denominator+=x2*x2+y2*y2+z2*z2;
-
-	// }
-
-	// ret.scale=numerator/denominator;
-
-
-	// return ret;
-// }//end register
-
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     [[maybe_unused]] auto& logger = init("logger");
@@ -290,6 +231,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     spdlog::info("Processing cloud(s)");
 
     pcl::PointCloud<pcl::PointXYZRGB> out_cloud;
+
+    std::vector<pcl::PointCloud<pcl::PointXYZRGB>> cyl_clouds;
 
     for (const auto& [idx, cloud] : std::views::enumerate(clouds)) {
         spdlog::info("Cloud size: {}", cloud.size());
@@ -318,7 +261,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
 
         const auto [quarter1, quarter2, quarter3, quarter4] = quarter_cloud(cloud_filtered);
 
-        pcl::PointCloud<pcl::PointXYZRGB> cylinders;
+        // pcl::PointCloud<pcl::PointXYZRGB> cylinders;
 
         auto futures = std::array {
             std::async(extract_cylinder, quarter1),
@@ -333,19 +276,75 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
         for (auto& f : futures) {
             const auto [params, cylinder, rest] = f.get();
             cyl_params.push_back(params);
-            cylinders += cylinder;
+            // cylinders += cylinder;
         }
 
-        out_cloud += cylinders;
+        // out_cloud += cylinders;
 
-        pcl::io::savePLYFile(fmt::format("./cyl-{}.ply", idx), cylinders);
+        // pcl::io::savePLYFile(fmt::format("./cyl-{}.ply", idx), cylinders);
 
         for (const auto& p : cyl_params) {
             fmt::print("{} {}\n", p.x(), p.y());
         }
+
+        pcl::PointCloud<pcl::PointXYZRGB> cylinders;
+
+        for (const auto& param : cyl_params) {
+            if (std::isnan(param.x()) or std::isnan(param.y()) or std::isnan(param.z()) or std::isnan(param.w())) {
+                continue;
+            }
+
+            const auto points = gen_circle(param);
+
+
+            for (const auto& p : points) {
+                cylinders.emplace_back(p.x(), p.y(), p.z(), 0, 255, 0);
+            }
+        }
+
+        out_cloud += cylinders;
+
+        // pcl::io::savePLYFile(fmt::format("./cylinders-{}.ply", idx), cylinders);
+
+        if (cyl_clouds.size() > 0) {
+            std::vector<nova::Vec3f> pts1, pts2;
+
+            const auto& prev_cloud = cyl_clouds.back();
+
+            for (std::size_t i = 0; i < std::min(std::size(prev_cloud), std::size(cylinders)); ++i) {
+                pts1.emplace_back(prev_cloud[i].x, prev_cloud[i].y, prev_cloud[i].z);
+                pts2.emplace_back(cylinders[i].x, cylinders[i].y, cylinders[i].z);
+            }
+
+            [[maybe_unused]] const auto trafo = register_(pts1, pts2);
+
+            pcl::PointCloud<pcl::PointXYZRGB> out;
+
+            for (const auto& p : prev_cloud) {
+                out.emplace_back(p.x - trafo.offset1.x(), p.y - trafo.offset1.y(), p.z - trafo.offset1.z(), 255, 0, 0);
+            }
+
+            for (const auto& p : cylinders) {
+                Eigen::Vector3f pt;
+                pt(0) = p.x - trafo.offset2.x();
+                pt(1) = p.y - trafo.offset2.y();
+                pt(2) = p.z - trafo.offset2.z();
+
+                const auto pp = (trafo.R * pt) * trafo.scale;
+
+                out.emplace_back(pp(0), pp(1), pp(2), 0, 255, 0);
+            }
+
+            pcl::io::savePLYFile("./registered.ply", out);
+
+            break;
+            // throw std::runtime_error("We'll stop here.");
+        }
+
+        cyl_clouds.push_back(cylinders);
     }
 
-    pcl::io::savePLYFile("./test.ply", out_cloud);
+    pcl::io::savePLYFile("./raw.ply", out_cloud);
 
     return EXIT_SUCCESS;
 }
