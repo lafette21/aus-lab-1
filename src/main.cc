16517003ca49ff9138f19548b9f0054e468a78f7
Ákos Virágh <viragh.akos21@gmail.com>
2024-05-02 09:55:33 +0200

    refact: remove pose calculation

--- src/main.cc
+++ src/main.cc
@@ -28,7 +28,7 @@ inline auto& init(const std::string& name) {
 }
 
 [[nodiscard]] auto extract_cylinder(const pcl::PointCloud<pcl::PointXYZRGB>& cloud)
-        -> std::pair<pcl::PointCloud<pcl::PointXYZRGB>, std::vector<nova::Vec3f>>
+        -> std::tuple<nova::Vec4f, pcl::PointCloud<pcl::PointXYZRGB>, std::vector<nova::Vec3f>>
 {
     auto tmp = cloud
              | std::views::transform([](const auto& elem) { return nova::Vec3f { elem.x, elem.y, elem.z }; })
@@ -51,27 +51,28 @@ inline auto& init(const std::string& name) {
     }
 
     return {
+        cylinder_params,
         cylinder,
         rest
     };
 }
 
-[[nodiscard]] auto calc_curr_pose(
-    float prev_velocity,
-    float prev_angular_velocity,
-    float prev_x,
-    float prev_y,
-    float prev_theta,
-    float prev_side_slipping = 0
-)
-        -> std::tuple<float, float, float>
-{
-    return {
-        prev_x + prev_velocity * std::cos(prev_theta + prev_angular_velocity / 2 + prev_side_slipping),
-        prev_y + prev_velocity * std::sin(prev_theta + prev_angular_velocity / 2 + prev_side_slipping),
-        prev_theta + prev_angular_velocity
-    };
-}
+// [[nodiscard]] auto calc_curr_pose(
+    // float prev_velocity,
+    // float prev_angular_velocity,
+    // float prev_x,
+    // float prev_y,
+    // float prev_theta,
+    // float prev_side_slipping = 0
+// )
+        // -> std::tuple<float, float, float>
+// {
+    // return {
+        // prev_x + prev_velocity * std::cos(prev_theta + prev_angular_velocity / 2 + prev_side_slipping),
+        // prev_y + prev_velocity * std::sin(prev_theta + prev_angular_velocity / 2 + prev_side_slipping),
+        // prev_theta + prev_angular_velocity
+    // };
+// }
 
 [[nodiscard]] auto quarter_cloud(const pcl::PointCloud<pcl::PointXYZRGB>& cloud)
         -> std::array<pcl::PointCloud<pcl::PointXYZRGB>, 4>
@@ -109,6 +110,161 @@ inline auto& init(const std::string& name) {
     };
 }
 
+struct trafo {
+    // R;
+    nova::Vec3f offset1;
+    nova::Vec3f offset2;
+    float scale;
+};
+
+auto register_(const auto& pts1, const auto& pts2)
+        -> trafo
+{
+    assert(pts1.size() <= pts2.size());
+
+    trafo ret;
+
+    const auto num_pts = pts1.size();
+
+    nova::Vec3f offset1{ 0, 0, 0 };
+    nova::Vec3f offset2{ 0, 0, 0 };
+
+    for (std::size_t i = 0; i < num_pts; ++i) {
+        const auto& p1 = pts1[i];
+        const auto& p2 = pts2[i];
+
+        offset1 += p1;
+        offset2 += p2;
+    }
+
+    offset1 /= num_pts;
+    offset2 /= num_pts;
+
+    ret.offset1 = offset1;
+    ret.offset2 = offset2;
+
+
+}
+
+// typedef struct Trafo{
+// Mat rot;
+// float scale;
+// Point3f offset1,offset2;
+// } Trafo;
+
+// Trafo registration(vector<Point3f>& pts1, vector<Point3f>& pts2){
+
+// Trafo ret;
+
+	// int NUM=pts1.size();
+
+	// //Subtract offsets
+
+	// Point3d offset1(0.0,0.0,0.0);
+	// Point3d offset2(0.0,0.0,0.0);
+
+	// for (int i=0;i<NUM;i++){
+		// Point3f v1=pts1[i];
+		// Point3f v2=pts2[i];
+
+		// offset1.x+=v1.x;
+		// offset1.y+=v1.y;
+		// offset1.z+=v1.z;
+
+		// offset2.x+=v2.x;
+		// offset2.y+=v2.y;
+		// offset2.z+=v2.z;
+	// }
+
+	// offset1.x/=NUM;
+	// offset1.y/=NUM;
+	// offset1.z/=NUM;
+
+	// offset2.x/=NUM;
+	// offset2.y/=NUM;
+	// offset2.z/=NUM;
+
+	// ret.offset1.x=offset1.x;
+	// ret.offset1.y=offset1.y;
+	// ret.offset1.z=offset1.z;
+
+	// ret.offset2.x=offset2.x;
+	// ret.offset2.y=offset2.y;
+	// ret.offset2.z=offset2.z;
+
+	// Mat H=Mat::zeros(3,3,CV_32F);
+	// for (int i=0;i<NUM;i++){
+		// Point3f v1=pts1[i];
+		// Point3f v2=pts2[i];
+
+		// float x1=v1.x-offset1.x;
+		// float y1=v1.y-offset1.y;
+		// float z1=v1.z-offset1.z;
+
+		// float x2=v2.x-offset2.x;
+		// float y2=v2.y-offset2.y;
+		// float z2=v2.z-offset2.z;
+
+		// H.at<float>(0,0)+=x2*x1;
+		// H.at<float>(0,1)+=x2*y1;
+		// H.at<float>(0,2)+=x2*z1;
+
+		// H.at<float>(1,0)+=y2*x1;
+		// H.at<float>(1,1)+=y2*y1;
+		// H.at<float>(1,2)+=y2*z1;
+
+		// H.at<float>(2,0)+=z2*x1;
+		// H.at<float>(2,1)+=z2*y1;
+		// H.at<float>(2,2)+=z2*z1;
+	// }
+
+
+	// Mat w(3,3,CV_32F);
+	// Mat u(3,3,CV_32F);
+	// Mat vt(3,3,CV_32F);
+
+	// SVD::compute(H,w,u, vt);
+
+	// Mat rot=vt.t()*u.t();
+	// ret.rot=rot;
+
+	// float numerator=0.0;
+	// float denominator=0.0;
+
+	// for (int i=0;i<NUM;i++){
+		// Point3f v1=pts1[i];
+		// Point3f v2=pts2[i];
+
+		// float x1=v1.x-offset1.x;
+		// float y1=v1.y-offset1.y;
+		// float z1=v1.z-offset1.z;
+
+
+		// Mat p2(3,1,CV_32F);
+
+		// p2.at<float>(0,0)=v2.x-offset2.x;
+		// p2.at<float>(1,0)=v2.y-offset2.y;
+		// p2.at<float>(2,0)=v2.z-offset2.z;
+
+		// p2=rot*p2;
+
+		// float x2=p2.at<float>(0,0);
+		// float y2=p2.at<float>(1,0);
+		// float z2=p2.at<float>(2,0);
+
+
+		// numerator+=x1*x2+y1*y2+z1*z2;
+		// denominator+=x2*x2+y2*y2+z2*z2;
+
+	// }
+
+	// ret.scale=numerator/denominator;
+
+
+	// return ret;
+// }//end register
+
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     [[maybe_unused]] auto& logger = init("logger");
 
@@ -116,8 +272,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
                     | std::views::transform([](const auto& arg) { return std::string_view {arg}; });
 
     constexpr std::size_t From = 165;
-    constexpr std::size_t To = 170;
-    constexpr std::size_t Speed = 1;
+    constexpr std::size_t To = 167;
 
     std::vector<pcl::PointCloud<pcl::PointXYZRGB>> clouds;
     clouds.reserve(To - From);
@@ -126,55 +281,18 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
 
     for (std::size_t i = From; i < To; ++i) {
         const auto cloud = read_file<lidar_data_parser>(
-            (std::filesystem::path(args[1]) / fmt::format("test_fn{}.xyz", i + (From - i) * Speed * 20)).string()
+            (std::filesystem::path(args[1]) / fmt::format("test_fn{}.xyz", i)).string()
         ).value();
         clouds.push_back(cloud);
     }
 
     spdlog::info("Read {} cloud(s)", clouds.size());
-
-    std::vector<float> velocities;
-    velocities.reserve(To - From);
-
-    for (std::size_t i = 0; i < To - From + 1; ++i) {
-        velocities.push_back(Speed);
-    }
-
-    std::vector<float> angular_velocities;
-    angular_velocities.reserve(To - From);
-
-    for (std::size_t i = 0; i < To - From + 1; ++i) {
-        angular_velocities.push_back(0);
-    }
-
-    std::vector<nova::Vec3f> poses;
-    poses.reserve(To - From);
-    poses.emplace_back(0, 0, 0);
-
     spdlog::info("Processing cloud(s)");
 
     pcl::PointCloud<pcl::PointXYZRGB> out_cloud;
 
     for (const auto& [idx, cloud] : std::views::enumerate(clouds)) {
         spdlog::info("Cloud size: {}", cloud.size());
-
-        // const auto [x, y, theta] = calc_curr_pose(
-            // velocities[static_cast<std::size_t>((static_cast<int>(idx) - 1) % static_cast<int>(velocities.size()))],
-            // angular_velocities[static_cast<std::size_t>((static_cast<int>(idx) - 1) % static_cast<int>(angular_velocities.size()))],
-            // poses[static_cast<std::size_t>((static_cast<int>(idx) - 1) % static_cast<int>(poses.size()))].x(),
-            // poses[static_cast<std::size_t>((static_cast<int>(idx) - 1) % static_cast<int>(poses.size()))].y(),
-            // poses[static_cast<std::size_t>((static_cast<int>(idx) - 1) % static_cast<int>(poses.size()))].z()
-        // );
-        const auto [x, y, theta] = calc_curr_pose(
-            velocities[static_cast<std::size_t>(idx)],
-            angular_velocities[static_cast<std::size_t>(idx)],
-            poses[static_cast<std::size_t>(idx)].x(),
-            poses[static_cast<std::size_t>(idx)].y(),
-            poses[static_cast<std::size_t>(idx)].z()
-        );
-        poses.emplace_back(x, y, theta);
-
-        spdlog::info("Current pose: x={} y={} theta={}", x, y, theta);
 
         pcl::PointCloud<pcl::PointXYZRGB> tmp_cloud;
         pcl::VoxelGrid<pcl::PointXYZRGB> vg;
@@ -209,22 +327,22 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
             std::async(extract_cylinder, quarter4)
         };
 
+        std::vector<nova::Vec4f> cyl_params;
+        cyl_params.reserve(4);
+
         for (auto& f : futures) {
-            const auto [cylinder, rest] = f.get();
+            const auto [params, cylinder, rest] = f.get();
+            cyl_params.push_back(params);
             cylinders += cylinder;
         }
 
-        Eigen::Affine3f transform = Eigen::Affine3f::Identity();
-
-        // Define a translation of 10 meters on the x axis.
-        transform.translation() << x, y, 0.0;
-
-        // The same rotation matrix as before; theta radians around Z axis
-        transform.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));
-
-        pcl::transformPointCloud(cylinders, cylinders, transform);
-
         out_cloud += cylinders;
+
+        pcl::io::savePLYFile(fmt::format("./cyl-{}.ply", idx), cylinders);
+
+        for (const auto& p : cyl_params) {
+            fmt::print("{} {}\n", p.x(), p.y());
+        }
     }
 
     pcl::io::savePLYFile("./test.ply", out_cloud);
