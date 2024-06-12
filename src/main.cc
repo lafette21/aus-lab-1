78ea9a07adb61d753771365fd2e2e7d4f9cd12e7
lafette21 <viragh.akos21@gmail.com>
2024-06-12 21:21:43 +0200

    Restructure again

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,11 @@
-#include "cylinder.hh"
+#include "simulator/logging.hh"
+#include "types.hh"
 #include "utils.hh"
 
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <charconv>
+#include "nova/io.h"
 #include <nova/vec.h>
 #include <nova/utils.h>
 #include <fmt/format.h>
@@ -25,70 +27,6 @@
 #include <span>
 #include <utility>
 
-
-inline auto& init(const std::string& name) {
-    spdlog::set_default_logger(spdlog::create<spdlog::sinks::ansicolor_stdout_sink_mt>(name));
-
-    auto& logger = *spdlog::get(name);
-    logger.set_pattern("[%Y-%m-%d %H:%M:%S.%f %z] [%n @%t] %^[%l]%$ %v");
-
-    return logger;
-}
-
-template <std::floating_point T = float>
-[[nodiscard]] auto gen_circle(T x, T y, T z, T r, std::size_t num_points = 100)
-        -> std::vector<nova::vec3<T>>
-{
-    std::vector<nova::vec3<T>> ret;
-    ret.reserve(num_points);
-
-    const auto angles = nova::linspace<T>(nova::range<T>{ 0.f, 2 * std::numbers::pi_v<T> }, num_points);
-
-    for (const auto& angle : angles) {
-        const auto x_ = x + r * std::cos(angle);
-        const auto y_ = y + r * std::sin(angle);
-
-        ret.emplace_back(x_, y_, z);
-    }
-
-    return ret;
-}
-
-template <std::floating_point T = float>
-[[nodiscard]] auto gen_circle(nova::vec4<T> params, std::size_t num_points = 100)
-        -> std::vector<nova::vec3<T>>
-{
-    return gen_circle(params.x(), params.y(), params.z(), params.w(), num_points);
-}
-
-[[nodiscard]] auto extract_cylinder(const pcl::PointCloud<pcl::PointXYZRGB>& cloud)
-        -> std::tuple<nova::Vec4f, pcl::PointCloud<pcl::PointXYZRGB>, std::vector<nova::Vec3f>>
-{
-    const auto points = cloud
-                      | std::views::transform([](const auto& elem) { return nova::Vec3f { elem.x, elem.y, elem.z }; })
-                      | std::views::filter([](const auto& elem) { return elem != nova::Vec3f { 0, 0, 0 }; })
-                      | ranges::to<std::vector>();
-
-    const auto cylinder_params = estimate_cylinder_RANSAC(points, 0.07f, 5'000);
-    const auto differences = calculate_RANSAC_diffs(points, cylinder_params, 0.07f);
-
-    pcl::PointCloud<pcl::PointXYZRGB> cylinder;
-    std::vector<nova::Vec3f> rest;
-
-    for (std::size_t i = 0; i < points.size(); ++i) {
-        if (differences.is_inliers.at(i)) {
-            cylinder.emplace_back(points[i].x(), points[i].y(), points[i].z(), 255, 0, 0);
-        } else {
-            rest.push_back(points[i]);
-        }
-    }
-
-    return {
-        cylinder_params,
-        cylinder,
-        rest
-    };
-}
 
 [[nodiscard]] auto quarter_cloud(const pcl::PointCloud<pcl::PointXYZRGB>& cloud)
         -> std::array<pcl::PointCloud<pcl::PointXYZRGB>, 4>
@@ -126,59 +64,6 @@ template <std::floating_point T = float>
     };
 }
 
-struct trafo {
-    Eigen::Matrix3f R;
-    Eigen::Vector3f t;
-};
-
-/*
- * @brief   Finding optimal rotation and translation between corresponding 3D points
- *
- * https://nghiaho.com/?page_id=671
- * https://github.com/nghiaho12/rigid_transform_3D/blob/master/rigid_transform_3D.py
- *
- */
-auto rigid_transform_3D(const Eigen::MatrixXf& A, const Eigen::MatrixXf& B)
-        -> trafo
-{
-    assert(A.rows() == B.rows() and A.cols() == B.cols());
-
-    if (A.rows() != 3) {
-        throw std::runtime_error("Matrix A is not 3xN!");
-    }
-
-    if (B.rows() != 3) {
-        throw std::runtime_error("Matrix B is not 3xN!");
-    }
-
-    trafo ret;
-
-    const Eigen::Vector3f centroid_A = A.rowwise().mean();
-    const Eigen::Vector3f centroid_B = B.rowwise().mean();
-
-    const Eigen::MatrixXf Am = A.colwise() - centroid_A;
-    const Eigen::MatrixXf Bm = B.colwise() - centroid_B;
-
-    const Eigen::Matrix3f H = Am * Bm.transpose();
-
-    Eigen::JacobiSVD<Eigen::Matrix3f> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
-
-    const Eigen::Matrix3f Ut = svd.matrixU().transpose();
-    Eigen::Matrix3f V = svd.matrixV();
-
-    ret.R = V * Ut;
-
-    // special reflection case
-    if (ret.R.determinant() < 0) {
-        V.col(2) *= -1;
-        ret.R = V * Ut;
-    }
-
-    ret.t = -ret.R * centroid_A + centroid_B;
-
-    return ret;
-}
-
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     [[maybe_unused]] auto& logger = init("logger");
 
@@ -197,7 +82,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     spdlog::info("Reading cloud(s)");
 
     for (std::size_t i = from; i < to; ++i) {
-        const auto cloud = read_file<lidar_data_parser>(
+        const auto cloud = nova::read_file<lidar_data_parser>(
             (std::filesystem::path(args[1]) / fmt::format("test_fn{}.xyz", i)).string()
         ).value();
         clouds.push_back(cloud);
